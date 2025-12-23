fd923bf38be1d88ea373939c6bdda17a8d2b5ef1
Tommy JOUHANS <12119@holbertonstudents.com>
2025-12-23 14:09:21 +0000

    modification de exe_cmd pour voir l'erreur du checkeur

--- exe_cmd.c
+++ exe_cmd.c
@@ -12,72 +12,69 @@
  */
 int exe_cmd(char *line, char **envp, char *line_buf)
 {
-	pid_t child;
-	size_t i;
-	char *cmd_path;
-	char **av;
-	int status;
-
-	av = split_line(line);
-	if (!av || !av[0])
-	{
-		if (av)
-			free(av);
-		return (0);
-	}
-
-	if (strcmp(av[0], "env") == 0)
-	{
-		status = builtin_env(envp);
-		for (i = 0; av[i]; i++)
-			free(av[i]);
-		free(av);
-		return (status);
-	}
-    if (av[0][0] == '/' || (av[0][0] == '.' && av[0][1] == '/'))
+pid_t child;
+size_t i;
+char *cmd_path = NULL;
+char **av;
+int status;
+av = split_line(line);
+if (!av || !av[0])
 {
-	cmd_path = strdup(av[0]);
+if (av)
+free(av);
+return (0);
+}
+if (strcmp(av[0], "env") == 0)
+{
+status = builtin_env(envp);
+for (i = 0; av[i]; i++)
+free(av[i]);
+free(av);
+return (status);
+}
+if (av[0][0] == '/' || (av[0][0] == '.' && av[0][1] == '/'))
+{
+cmd_path = strdup(av[0]);
 }
 else
 {
 cmd_path = find_in_path(av[0], envp);
 }
 if (!cmd_path)
-	{
-		fprintf(stderr, "%s: %d: %s: not found\n", SHELL_NAME, 1, av[0]);
-		for (i = 0; av[i]; i++)
-			free(av[i]);
-		free(av);
-		return (127);
-	}
-
-	child = fork();
-	if (child < 0)
-	{
-		perror("fork");
-		free(cmd_path);
-		for (i = 0; av[i]; i++)
-			free(av[i]);
-		free(av);
-		return (1);
-	}
-	if (child == 0)
-	{
-		execve(cmd_path, av, envp);
-		perror("execve");
-		free(cmd_path);
-		for (i = 0; av[i]; i++)
-			free(av[i]);
-		free(av);
-		free(line_buf);
-		_exit(126);
-	}
-	free(cmd_path);
-	for (i = 0; av[i]; i++)
-		free(av[i]);
-	free(av);
-	waitpid(child, &status, 0);
-	if (WIFEXITED(status))
-		return (WEXITSTATUS(status));
-	return (1);
+{
+fprintf(stderr, "%s: %d: %s: not found\n", SHELL_NAME, 1, av[0]);
+for (i = 0; av[i]; i++)
+free(av[i]);
+free(av);
+return (127);
+}
+child = fork();
+if (child < 0)
+{
+perror("fork");
+free(cmd_path);
+for (i = 0; av[i]; i++)
+free(av[i]);
+free(av);
+return (1);
+}
+if (child == 0)
+{
+execve(cmd_path, av, envp);
+perror("execve");
+free(cmd_path);
+for (i = 0; av[i]; i++)
+free(av[i]);
+free(av);
+free(line_buf);
+_exit(126);
+}
+waitpid(child, &status, 0);
+free(cmd_path);
+for (i = 0; av[i]; i++)
+free(av[i]);
+free(av);
+if (WIFEXITED(status))
+return (WEXITSTATUS(status));
+return (1);
 }
